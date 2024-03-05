583f6adee05fbdf397e7efd84460af200277b7ec
Trevor Ablett <trevor.ablett@gmail.com>
2024-03-05 13:34:29 -0500

    Add a new visualization script based on raw datasets from hand_dapg

--- scripts/visualize_dataset.py
+++ scripts/visualize_dataset.py
@@ -6,10 +6,11 @@ import gym
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--env_name', type=str, default='maze2d-umaze-v0')
+    parser.add_argument('--use_acts', action='store_true')
     args = parser.parse_args()
 
     env = gym.make(args.env_name)
-    
+
     dataset = env.get_dataset()
     if 'infos/qpos' not in dataset:
         raise ValueError('Only MuJoCo-based environments can be visualized')
@@ -17,9 +18,16 @@ if __name__ == "__main__":
     qvel = dataset['infos/qvel']
     rewards = dataset['rewards']
     actions = dataset['actions']
+    dones = dataset['terminals']
 
     env.reset()
     env.set_state(qpos[0], qvel[0])
     for t in range(qpos.shape[0]):
-        env.set_state(qpos[t], qvel[t])
+        if args.use_acts:
+            obs, rew, done, info = env.step(actions[t])
+            if dones[t]:
+                env.reset()
+                env.set_state(qpos[t+1], qvel[t+1])
+        else:
+            env.set_state(qpos[t], qvel[t])
         env.render()
